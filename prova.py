fbb7d9b707cb2bb74371f1569a788d7645a92304
StuartKarro <110871602+StuartKarro@users.noreply.github.com>
2022-11-17 03:21:34 -0300

    Update prova.py

--- prova.py
+++ prova.py
@@ -1,60 +1,109 @@
 1)
-valor1 = int(input("Digite um valor"))
-valor2 = int(input("Digite outro valor"))
-valor3 = int(input("Digite outro valor"))
-if valor1 > valor2 and valor1 > valor3:
-    print(valor1)
-elif valor2 > valor1 and valor2 > valor3:
-    print(valor2)
-else:print(valor3)
+def fazerlista(tamanho):
+    lista = list()
+    for l in range(tamanho):
+        lista.append(0)
+    return lista
+
+
+tamanho = int(input('Insira o tamanho da lista'))
+lista = fazerlista(tamanho)
+for l in range(len(lista)):
+    numero = int(input("Insira um número da lista: "))
+    lista[l] = numero
+
+ordem = sorted(lista)
+soma = sum(lista)
+maiornumero = max(lista)
+
+print("Lista.", lista)
+print(" lista ordenada.", ordem)
+print("A soma dos valores.", soma)
+print("O maior número da lista.", maiornumero)
 
 2)
-idade = int(input("Digite a sua idade"))
-tempoTrabalho = int(input("Digite o tempo de trabalho em anos"))
+dado = [1,2,3,4,5,6]
+player1 = list()
+player2 = list()
+verificar = 0
+from random import  randint
 
-if idade >= 65:
-    print("Pode se aposentar")
-elif idade < 65 and tempoTrabalho >= 25:
-    print("Pode se apresentar")
-elif tempoTrabalho >= 30:
-    print("Pode se aposentar")
+
+while True:
+    opcao = int(input("[ 1 ] - Jogar [ 2 ] - Sair \nDigite 1 para jogar e 2 para sair: "))
+
+    if opcao == 1:
+        if verificar % 2 == 0:
+            player1.append(dado[randint(0,5)])
+            print("Número do Player1:",(player1[-1]))
+            verificar += 1
+        
+        else:
+            player2.append(dado[randint(0,5)])
+            print("Número do Player2",(player2[-1]))
+            verificar += 1
+            
+    else:
+        if verificar % 2 != 0:
+            player2.append(dado[randint(0, 5)])
+            print("Último número do jogador 2", (player2[-1]))
+        break
+
+soma = sum(player1)
+soma1 = sum(player2)
+print("\nResultados: ")
+print("Jogadas do jogador 1:", player1)
+print("Jogadas do jogador 2:", player2)
+print("Soma dos valores do jogador 1:", soma)
+print("Soma dos valores do jogador 2:", soma1)
+if sum(player1) > sum(player2):
+    print("O jogador 1 é o vencedor")
 else:
-    print("Não pode se aposentar")
+    print("O jogador 2 é o vencedor")
+print("Obrigada por Jogar!")
+
 
 3)
-import random
-import string
+def fazermatriz(linha, coluna):
+    lista = list()
+    for i in range(linha + 1):
+        lista.append([0] * coluna)
+    return lista
+
+
+linha = int(input("Digite o número de linhas:"))
+coluna = int(input("Digite o número de colunas: "))
+matriz = fazermatriz(linha, coluna)
 contador = 0
-letra = random.choice(string.ascii_letters)
-print("Você tem apenas 5 chances para adivinhar a letra")
-while contador <= 4:
-    chance = input("Digite uma letra")
-    if chance == letra:
-        print("Acertou")
-    else:
-        contador = contador +1
-print("Você não acertou, a letra era",letra)
+
+for l in range(linha):
+    for c in range(coluna):
+        contador += 1
+        indices = linha * coluna
+        numero = int(input("Digite", indices, "numeros"))
+        matriz[l][c] = numero
+  
+
+
 
 4)
-saldo = 0
-menu = 0
-print("bem-vindo")
-while menu != 4:
-    menu = int(input("Digite 1 para consultar o saldo \n"
-                 "Digite 2 para saque \n"
-                 "Digite 3 para depósito \n"
-                 "Digite 4 para sair"))
+linha = 5
+coluna = 5
 
-    if menu == 1:
-        print(float(saldo))
-    elif menu == 2:
-        saque = float(input("Digite o valor do saque"))
-        if saque > saldo:
-            print("saldo indisponivel")
-        else:
-            saldo = saldo - saque
-            print(float("saldo"))
-    elif menu == 3:
-        deposito = float(input("Digite o valor do deposito"))
-        saldo = saldo + deposito
-        print(saldo)
+def fazermatriz(linha, coluna):
+    lista = list()
+    for i in range(linha + 1):
+        lista.append([0] * coluna)
+    return lista
+
+
+matriz = fazermatriz(linha, coluna)
+
+
+for l in range(linha):
+    for c in range(coluna):
+        if l == c:
+            matriz[l][c] = 1
+
+for i in range(linha):
+    print(matriz[i])
